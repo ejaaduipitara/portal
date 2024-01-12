ff76770bbe09563faa6a2a59a7a8845a43c453e2
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2024-01-12 12:11:30 +0530

    logo and title change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
+    title: window['env']['title'],
     host: window['env']['host'],
     authToken: window['env']['authToken'],
     superSetDomain: window['env']['superSetDomain'],
